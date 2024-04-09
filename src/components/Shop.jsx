40b31f924a5002a47fe199b5a446f4343ede9f55
barbashovad232@mail.ru <khananna42@gmail.com>
2024-04-09 18:29:14 +0300

    Initial commit

---
+++ src/components/Shop.jsx
@@ -0,0 +1,123 @@
+import { useState, useEffect } from "react";
+import { API_KEY, API_URL } from "../config";
+import { Preloader } from "./preloader";
+import { GoodsList } from "./goodsList";
+import { Cart } from "./cart";
+import { BasketList } from "./BasketList";
+import { Alert } from "./alert";
+
+function Shop() {
+
+    const [goods, setGoods] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [order, setOrder] = useState([]);
+    const [isBasketShow, setBasketShow] = useState(false);
+
+    const [ alertName, setAlertName] = useState("");
+
+    const handleBasketShow = () => {
+        console.log('handleBasketShow');
+        setBasketShow(!isBasketShow);
+    };
+    const closeAlert = () => {
+        setAlertName("");
+    };
+
+    const addToBasket = (item) => {
+        const itemIndex = order.findIndex((orderItem) => orderItem.id ===item.id);
+    
+        if (itemIndex < 0) {
+            const newItem = {
+                ...item,
+                quantity: 1,
+            };
+            setOrder ([...order, newItem]);
+        } else {
+            const newOrder = order.map((orderItem, index) => {
+                if (index === itemIndex) {
+                    return {
+                        ...orderItem,
+                        quantity: orderItem.quantity + 1,
+                    };
+                } else {
+                    return orderItem;
+                }
+            });
+    
+            setOrder(newOrder);
+        }
+        setAlertName(item.name);
+    };
+    
+    const removeItem = (itemId) => {
+        const newOrder = order.filter((el)=> el.id !== itemId);
+        setOrder(newOrder);
+    }
+
+    const incQuantity = (itemId) => {
+        const newOrder = order.map((el) => {
+            if (el.id === itemId) {
+                const newQuantity = el.quantity + 1;
+                return {
+                    ...el,
+                    quantity: newQuantity,
+                }
+            } else {
+                return el;
+            }
+        });
+        setOrder(newOrder);
+    };
+    const decQuantity = (itemId) => {
+        const newOrder = order.map((el) => {
+            if (el.id === itemId) {
+                const newQuantity = el.quantity - 1;
+                return {
+                    ...el,
+                    quantity: newQuantity,
+                }
+            } else {
+                return el;
+            }
+        });
+        setOrder(newOrder);
+    };
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", API_KEY);
+
+    const requestOptions = {method: "GET", headers: myHeaders};
+
+    useEffect(function getGoods() {
+        fetch(API_URL, requestOptions
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                data.shop && setGoods(data.shop);
+                setLoading(false);
+            })
+            .catch((error) => console.error(error));
+    }, []);
+    return (
+    <main className="container content">
+        <h1 className="center">Fortnite Shop</h1>
+        <h5 className="center">Здесь вы можете приобрести различные товары для игры в Fortnite</h5>
+        <h1></h1>
+        <Cart quantity = {order.length} handleBasketShow ={handleBasketShow}/>
+        {loading ? (
+            <Preloader/> 
+        ) : ( 
+            
+            <GoodsList goods = {goods} addToBasket = {addToBasket} /> 
+        )}
+        {isBasketShow && (
+        <BasketList 
+        order = {order} 
+        handleBasketShow = {handleBasketShow} 
+        removeItem = {removeItem}
+        incQuantity = {incQuantity}
+        decQuantity = {decQuantity}/>)}
+        {alertName && <Alert name = {alertName} closeAlert={closeAlert}/>}
+    </main>
+    )
+}
+export {Shop};
---
+++ src/components/header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+    return (
+       <nav className="page-header  #4a148c purple darken-4">
+          <div className="container">
+             <div className="nav-wrapper">
+                <a href="" className="brand-logo">
+                  Fortnite Shop
+                </a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                   <li>
+                      <a href="!#">Repository</a>
+                   </li>
+                </ul>
+             </div>
+          </div>
+       </nav>
+    );  
+ }
+ export {Header};
